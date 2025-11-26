573073e4ae45ebfa2eca56b317579807aab6d76e
Rajdeep Dutta <34247617+Rajdeepdutta192@users.noreply.github.com>
2025-11-27 00:16:29 +0530

    auth_setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+// Login page
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/login', [AuthController::class, 'login'])->name('admin.loginSubmit');
+
+// Dashboard (protected)
+Route::get('/dashboard', function () {
+    return view('admin.dashboard'); // create this view
+})->middleware('auth')->name('dashboard');
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
